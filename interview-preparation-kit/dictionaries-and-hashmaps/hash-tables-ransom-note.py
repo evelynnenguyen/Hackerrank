d52f09f7743cdd7c98f028acb751dcbc50914ddd
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-04-18 16:11:25 +1200

    Done ransom note

--- interview-preparation-kit/dictionaries-and-hashmaps/hash-tables-ransom-note.py
+++ interview-preparation-kit/dictionaries-and-hashmaps/hash-tables-ransom-note.py
@@ -5,19 +5,29 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 
 # Complete the checkMagazine function below.
 def checkMagazine(magazine, note):
+    mapM = Counter(magazine)
+    mapN = Counter(note)
+    a = mapN - mapM
+    if a == {}:
+        print("Yes")
+    else:
+        print("No")
 
 if __name__ == '__main__':
-    mn = input().split()
+    k = int(input())
+    for i in range(k):
+        mn = input().split()
 
-    m = int(mn[0])
+        m = int(mn[0])
 
-    n = int(mn[1])
+        n = int(mn[1])
 
-    magazine = input().rstrip().split()
+        magazine = input().rstrip().split()
 
-    note = input().rstrip().split()
+        note = input().rstrip().split()
 
-    checkMagazine(magazine, note)
+        checkMagazine(magazine, note)
