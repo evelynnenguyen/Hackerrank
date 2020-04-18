6770c1c36081bac251cc6719fb12fc9e915177db
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-04-18 16:32:45 +1200

    done two strings

---
+++ interview-preparation-kit/dictionaries-and-hashmaps/two-strings.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    m1 = set(s1)
+    m2 = set(s2)
+    if set.intersection(m1,m2):
+        return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        print(result)
